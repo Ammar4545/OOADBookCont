f94f8d8b7a8fd1519b6cd89db797729e135bd5cb
Ammar4545 <ammar.m777@gmail.com>
2024-10-10 09:50:08 +0300

    add DogDoor Class

---
+++ DogDoorUseCases/DogDoor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DogDoorUseCases
+{
+    public class DogDoor
+    {
+        private bool open;
+        public DogDoor()
+        {
+            this.open = false;
+        }
+        public void Open()
+        {
+            Console.WriteLine("The dog door opens");
+        }
+        public void Close()
+        {
+            Console.WriteLine("The dog door closes");
+        }
+        public bool IsOpen() => this.open;
+    }
+}
